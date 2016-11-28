a20018672d171148251dfec9a05bb3b257ba5419
a-spo <aspowart6@gmail.com>
2016-11-28 18:50:34 -0500

    Revert "Implemented control redundancy"
    This reverts commit 4ce03a023c04ecec425eb1e01f3b4136530999fa.

--- IntegratedTrainSystem/src/com/harambe/traincontroller/TrainState.java
+++ IntegratedTrainSystem/src/com/harambe/traincontroller/TrainState.java
@@ -1,8 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.harambe.traincontroller;
 
 
 import com.harambe.trainmodel.Train;
 
+/**
+ *
+ * @author Alex
+ */
 public class TrainState {
     
     private int trainID;
