278019b360653480bcbdce34b728f4ec26626bc2
ChadPongratz <cap121@pitt.edu>
2016-11-30 16:21:10 -0500

    Add files via upload

--- IntegratedTrainSystem/src/com/harambe/trackmodel/TrackModelPrototypeUI.java
+++ IntegratedTrainSystem/src/com/harambe/trackmodel/TrackModelPrototypeUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package My.TrackModel;
+package com.harambe.trackmodel;
 
 //import com.sun.istack.internal.logging.Logger;
 import java.io.*;
